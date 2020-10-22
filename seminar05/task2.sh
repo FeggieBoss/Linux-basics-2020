5c1a884cacdbc375ba804937d3111669ab4989a6
FeggieBoss <tyuryaev2002@mail.ru>
2020-10-23 01:20:18 +0300

    publish hw5

--- seminar05/task2.sh
+++ seminar05/task2.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+a=$1
+b=$2
+
+git log --pretty=format:"%H" >> hash.txt
+git log --pretty=format:"%s" >> text.txt
+
+l=0
+r=0
+
+i=0
+while read hash; do
+    if [ "$hash" == "$a" ]; then
+        l=$i
+    fi
+
+    if [ "$hash" == "$b" ]; then
+        r=$i
+    fi
+        
+    i=$((i+1))
+done < hash.txt
+
+i=0
+while read line; do
+    if [[ $l -le $i  ]]; then
+        if [[ $i -le $r ]]; then
+            echo "$line"
+        fi
+    fi
+
+    (( i++ ))
+done < text.txt
+
+
+rm hash.txt
+rm text.txt
--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+a='^issue-[0-9]* .*'
+
+if ! grep -qE "$a" "$1"; then
+	echo "Dude wrong commit message format."
+	exit 1
+fi
